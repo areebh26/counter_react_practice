6bd67458575c31564580945334a32446ac616e28
areeb <areebulhaq2005@gmail.com>
2026-01-16 07:26:28 +0500

    visual layout completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import Button from './components/button.jsx'
+import { useState } from 'react'
+function App(){
   return (
     <>
+      <h1>Counter is </h1>
+      <p>Count is </p>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Button text="increase"></Button>
+        <Button text="decrease"></Button>
+        <Button text="reset"></Button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <input type="text" value="0"/>
+      <Button text="Set to "></Button>
     </>
   )
 }
+
+
 
 export default App
